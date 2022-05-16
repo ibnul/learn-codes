32cccd0527af36b5f6f1cb89a2fde7146682dbc7
Ibnul Hasan <ibnul.ku.cse@gmail.com>
2022-05-16 12:37:07 +0600

    initial upload

---
+++ app.js
@@ -0,0 +1,34 @@
+const text = document.querySelector(".fancy");
+const strText = text.textContent;
+const splitText = strText.split("");
+
+text.textContent = "";
+//console.log(splitText);
+for( let i=0; i < splitText.length; i++ ){
+    text.innerHTML += "<span>" + splitText[i] + "</span>";
+}
+
+
+let char = 0;
+let timer = setInterval( onClick, 30 );
+
+function onClick(){
+    const span = text.querySelectorAll('span')[char];
+    span.classList.add('fade');
+
+    char++;
+
+    if(char == splitText.length ){
+        complete();
+        return;
+    }
+
+}
+
+function complete(){
+    clearInterval(timer);
+    timer = null;
+    setTimeout
+    setTimeout(myfunction, 3000);
+}
+
